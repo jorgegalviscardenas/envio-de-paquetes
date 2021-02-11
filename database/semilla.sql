3d36e22b5239caceaec7698ff3a6fce98cbaeb63
Alejo Gomez <alego17@gmail.com>
2021-02-10 20:20:59 -0500

    se adiciona script de semilla para la base de datos

---
+++ database/semilla.sql
@@ -0,0 +1,7 @@
+-- Insert de estados
+INSERT INTO estado VALUES(1,"Generado",1);
+INSERT INTO estado VALUES(2,"Asignado",2);
+INSERT INTO estado VALUES(3,"Recogido",3);
+INSERT INTO estado VALUES(4,"Camino",4);
+INSERT INTO estado VALUES(5,"Destino",5);
+INSERT INTO estado VALUES(6,"Entregado",6);
